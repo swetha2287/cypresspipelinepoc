cb3963b3f56ce8634af244420c6683f17e75c6df
AzureAD\SwethaAnguluri <swetha@iamhelium.com.au>
2025-08-14 12:50:11 +1000

    reverting back to cypress multi reports

--- cypress.config.js
+++ cypress.config.js
@@ -7,38 +7,16 @@ module.exports = defineConfig({
   
    
     
-  reporter: 'cypress-mochawesome-reporter',
-  
+reporter: 'cypress-multi-reporters',
   reporterOptions: {
-    // --- Global options for the final HTML report ---
-    charts: true,
-    reportPageTitle: 'Cypress Test Execution Report',
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    saveAllAttempts: false,
+    configFile: 'reporter-config.json'
+  },  
 
-    // --- Sub-reporter configuration ---
-    reporterEnabled: 'mochawesome, mocha-junit-reporter',
-
-    // Mochawesome saves its temp JSON files here
-    mochawesomeReporterOptions: {
-      reportDir: 'cypress/results/mochawesome',
-      overwrite: false,
-      html: false,
-      json: true,
-    },
-    
-    // JUnit saves its XML files here
-    mochaJunitReporterReporterOptions: {
-      mochaFile: 'cypress/results/junit/results-[hash].xml',
-    },
-  },
-  
   e2e: {
     setupNodeEvents(on, config) {
       // 3. The Mochawesome plugin is STILL needed here.
       // Its job is now simply to merge the HTML report at the end.
-     // require('cypress-mochawesome-reporter/plugin')(on);
+     require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
